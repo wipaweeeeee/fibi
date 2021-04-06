ce506bd684b2a4ae3fb0c083ea5a692a49d57423
wipaweeeeee <wsirikolkarn@gmail.com>
2021-04-06 00:22:31 -0400

    Updates

---
+++ precache-manifest.0a41b8e2eed44373f07cb956cef2089c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4705b1d13497d4d25785abb1072e30",
+    "url": "/fibi/index.html"
+  },
+  {
+    "revision": "33198f8e38204fa709c6",
+    "url": "/fibi/static/css/main.60af6a45.chunk.css"
+  },
+  {
+    "revision": "2e4b0af608d6170faaf9",
+    "url": "/fibi/static/js/2.b0ee2ca9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fibi/static/js/2.b0ee2ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33198f8e38204fa709c6",
+    "url": "/fibi/static/js/main.d9d2fb1f.chunk.js"
+  },
+  {
+    "revision": "87d9ebb7a5b4f7328cdd2990d2a3566e",
+    "url": "/fibi/static/js/mapbox-gl-csp-worker.f7c6d5a2.worker.js"
+  },
+  {
+    "revision": "feb39f08414eb5b2dd35",
+    "url": "/fibi/static/js/runtime-main.b80d36e1.js"
+  },
+  {
+    "revision": "5f2ce7df2a2e8570f4c32a44414df347",
+    "url": "/fibi/static/media/Inter-Black.5f2ce7df.ttf"
+  },
+  {
+    "revision": "515cae74eee4925d56e6ac70c25fc0f6",
+    "url": "/fibi/static/media/Inter-Regular.515cae74.ttf"
+  },
+  {
+    "revision": "ec60b23f3405050f546f4765a9e90fec",
+    "url": "/fibi/static/media/Inter-SemiBold.ec60b23f.ttf"
+  },
+  {
+    "revision": "7ff054510a891ec4bfd4085a15f65896",
+    "url": "/fibi/static/media/about_bg.7ff05451.svg"
+  },
+  {
+    "revision": "d62a34285c77adb66645c8f02b4b7d90",
+    "url": "/fibi/static/media/about_circle.d62a3428.svg"
+  },
+  {
+    "revision": "a46629734c42ae0d57091c4515636fdc",
+    "url": "/fibi/static/media/about_curve.a4662973.svg"
+  },
+  {
+    "revision": "2b9775f28fd00fdf51df2c0ce09f67b5",
+    "url": "/fibi/static/media/about_rec.2b9775f2.svg"
+  },
+  {
+    "revision": "029ea3f20b5d387d5f003fecb7dbbcfb",
+    "url": "/fibi/static/media/blogspot.029ea3f2.svg"
+  },
+  {
+    "revision": "6dde48691e437a63b94c7ae135c08afa",
+    "url": "/fibi/static/media/career_arrow.6dde4869.svg"
+  },
+  {
+    "revision": "817a8813b3d387e618456671caaf4fdd",
+    "url": "/fibi/static/media/clients_bg.817a8813.svg"
+  },
+  {
+    "revision": "cde4c83127b7d7cfff82cf8eccc0305e",
+    "url": "/fibi/static/media/close.cde4c831.svg"
+  },
+  {
+    "revision": "599eb2f9d4469137b5aebcd7f740aabd",
+    "url": "/fibi/static/media/fb.599eb2f9.svg"
+  },
+  {
+    "revision": "c35aed6c3d6e7294320f5b1a5b9f48c3",
+    "url": "/fibi/static/media/general.c35aed6c.svg"
+  },
+  {
+    "revision": "ed7e335e574032486792a65186154544",
+    "url": "/fibi/static/media/header.ed7e335e.gif"
+  },
+  {
+    "revision": "76f74cb14be0d245d2332e0cf2c6520f",
+    "url": "/fibi/static/media/header_video.76f74cb1.mp4"
+  },
+  {
+    "revision": "86114a2b8886f5f1c716ed4e42e262ee",
+    "url": "/fibi/static/media/joinus.86114a2b.svg"
+  },
+  {
+    "revision": "7b7976b411fec2cff168e5b748e29161",
+    "url": "/fibi/static/media/line.7b7976b4.svg"
+  },
+  {
+    "revision": "5179e0c0c28cd2cef561f08853709d5c",
+    "url": "/fibi/static/media/logo.5179e0c0.svg"
+  },
+  {
+    "revision": "6096939a8b0d88a3915d429a12b17640",
+    "url": "/fibi/static/media/menu.6096939a.svg"
+  },
+  {
+    "revision": "1d591f600de15c193c9af95e56e5129d",
+    "url": "/fibi/static/media/service_bg.1d591f60.svg"
+  }
+]);
